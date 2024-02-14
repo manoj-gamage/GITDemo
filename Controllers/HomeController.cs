36a409f6fbadfbb0192cc3925a8d77b824090a69
Manoj <manoj.gamage@qtcm.com>
2024-02-14 12:52:08 -0600

    3 rd change

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,6 +16,7 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         ViewBag.Message = "Lahiru will come back";
+        ViewBag.Message = "Lahiru will come back";
         return View();
     }
 
